bfbeb1677b3f04353a83d207e0f7544eb8c13c69
Odalis Garcia Vino <odalisgarcia7736@gmail.com>
2020-10-29 22:21:36 -0400

    Clase que crea un archivo de texto, y escribe los mensajes almacenados en la base de datos, en la tabla Chat.

---
+++ Downloader.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyect;
+
+import java.io.FileWriter;
+import java.sql.ResultSet;
+
+/**
+  * @author Odalis Garcia Vino
+ */
+public class Downloader {
+    ResultSet rst;
+    FileWriter file;
+    Conexion conn;
+    Downloader(){
+        //file = new File("fileChat.txt");
+        conn = new Conexion();
+    }
+    
+    public void downloadChat(){
+        String hour, username, message;
+        try {
+            file = new FileWriter("fileChat.txt");
+            ResultSet rst = conn.getResultSet();
+            while(rst.next()){
+                hour     = rst.getString("hour");
+                username = rst.getString("username");
+                message  = rst.getString("message");
+                
+                file.write(hour + "-" + username + ":" + "\n");
+                file.write(message +"\n");
+                file.write("\n");
+        }
+                    
+        file.close();
+
+        }catch(Exception ex){
+            System.out.println("No ha sido posible escribir en el fichero: " + ex.getMessage());
+	}
+        
+    }
+    public void cancelDownload(){
+        try{
+            FileWriter file = new FileWriter("fileChat.txt");
+            file.write("");
+            file.close();
+        }catch(Exception ex){
+            System.out.println("No ha sido posible escribir en el fichero: " + ex.getMessage());
+	}
+    }
+}
